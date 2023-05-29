70d4d8332a7155b247f18811ec10742ade6676d2
Arghya Das <arghyadasproject@gmail.com>
2023-05-29 16:35:25 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,6 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    /*   fontFamily: {
-      clash: ["ClashDisplay-Variable", "sans-serif"],
-      cabinet: ["CabinetGrotesk-Variable", "sans-serif"],
-      melodrama: ["Melodrama-Variable", "sans-serif"],
-    }, */
     extend: {},
   },
   plugins: [],
